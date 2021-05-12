411710fda03965a29a38155a8162bc63336bb2a5
karanOO89 <gk.987680@gmail.com>
2021-05-12 13:22:30 -0600

    appointment first components added

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -139,4 +142,7 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header",() => <Header time="12pm" />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
-  .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Show", () => <Show interviewer={interviewers[0]}student ={"Lydia Miller-Jones"}onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Confirm", () => <Confirm message="Delete this appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
+  .add("Status", () => <Status message ="Deleting" />)
+  .add("Error", () => <Error title="Error" message ="Could not delete appointment." onClose={action("onClose")} />)
